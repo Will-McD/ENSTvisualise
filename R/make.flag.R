fd8f88609843861ad8c92db71cac2096ca1e931e
Will-McD <77323079+Will-McD@users.noreply.github.com>
2021-01-22 16:48:27 +0800

    Update make.flag.R

--- R/make.flag.R
+++ R/make.flag.R
@@ -3,9 +3,9 @@ make.flag = function(){
 
   #'
   #'Creates the data.table which the flagged cells are stored in.
-  #'
+  #' @importFrom data.table data.table
   #'
   #'@export
 
-  Flagged <<- data.table('n' = double(), 'index' = double(), 'population' = double())
+  Flagged <<- data.table::data.table('n' = double(), 'index' = double(), 'population' = double())
 }
