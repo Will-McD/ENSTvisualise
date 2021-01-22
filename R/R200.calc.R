e2c49f5d6a3960b2e0879583a2d8702ef59681ba
Will-McD <77323079+Will-McD@users.noreply.github.com>
2021-01-22 14:45:07 +0800

    Update R200.calc.R

--- R/R200.calc.R
+++ R/R200.calc.R
@@ -18,14 +18,14 @@ R200.calc = function(species = halo$particles$species, mass.frac = c(1.17e8, 6.2
   #' particle within the halo in simulation units (solar_mass / h).
   #'Naturally set to the mass fraction from SURFS L210N1024NR.
   #'
-  #'(baryons, dark matter)
+  #'              (Baryons, Dark Matter)
   #'
   #'@export
   #'
 
   G = (6.67430e-11) * (3.403679e-59) / (5.02785e-31) # [Mpc^3 / (Solar Mass) / s^2]
   Ht = 70 * 3.2408e-20 # hubble const assumed as H_0 = 70 km/s Mpc converted to [Hz]
-  p.mass = mass.frac # mass of particles in simulation, (DM, Baryon) [Solar Mass / h]
+  p.mass = mass.frac # mass of particles in simulation, (Baryon, DM) [Solar Mass / h]
   Mt = sum(p.mass[species]) # total mass of particles within the halo [Solar Mass/ h]
 
   R200 = (( Mt * G) / (100 * Ht^2)) ^ (1/3) * 1e-3 # the virial radius defined as R_200 in simulation units [Mpc/h]
