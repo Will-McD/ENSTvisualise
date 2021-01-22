51a72a2ed3a281bcd040331204e130e3b2841a9d
Will-McD <77323079+Will-McD@users.noreply.github.com>
2021-01-22 15:03:11 +0800

    Update time_ff.R

--- R/time_ff.R
+++ R/time_ff.R
@@ -4,7 +4,7 @@ time_ff = function(species = halo$particles$species){
   #'
   #' Free Fall Time in Gyrs
   #'
-  #' NOT FINISHED YET !
+  #'  !!! NOT FINISHED YET !!!
   #'
   #'@export
   #'
@@ -14,8 +14,8 @@ time_ff = function(species = halo$particles$species){
   R = R200.calc() #[Mpc/h]
   G = (6.67430e-11) * (3.403679e-59) / (5.02785e-31) # [Mpc^3 / (Solar Mass) / s^2]
   Ht = 70 * 3.2408e-20 # hubble const assumed as H_0 = 70 km/s Mpc, convert to [Hz]
-  p.mass = c(1.17e8, 6.29e8) # mass of particles in simulation, (DM, Baryon) [Solar Mass / h]
+  p.mass = c(1.17e8, 6.29e8) # mass of particles in simulation, (Baryon, Dark Matter) [Solar Mass / h]
   M.t = sum(p.mass[species]) # total mass of particles within the halo [Solar Mass/ h]
-  t.ff = 0.5 * (pi * R^(3/2)) / sqrt(2*G*(M.t)) # free fall time [s]
+  t.ff = 0.5 * (pi * R^(3/2)) / sqrt(2*G*(M.t)) 
   return(t.ff/cst$yr/1e9) #[Gyrs]
 }
